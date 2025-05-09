97914ff3f6e8039c5c68a89cd3b30f6b64197b24
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-05-09 16:40:37 +0200

    deps: add crxjs and tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    crx({ manifest }),
+  ],
 })
